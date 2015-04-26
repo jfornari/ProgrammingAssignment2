4b0eb94337bea83dd7f4ee0e98319e86b91a3506
Javier Fornari <javier@fornari.org>
2015-04-26 19:56:22 -0300

    listo

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions use for R.Programming
 
-## Write a short comment describing this function
+## Creates a special "vector"
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
 }
 
 
-## Write a short comment describing this function
+## Mean calculation of the special "vector"
+
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
+}
+
+
+## Creates an inverse matrix to use as cache
+
+makeCacheMatrix <- function(x = matrix()) {
+  intermedio <- NULL
+  set <- function(y) {
+    x <<- y
+    intermedio <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) intermedio <<- solve
+  getsolve <- function() intermedio
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+}
+
+
+## Solve the inverse of a cache matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    datos <- x$getsolve()
+    if(!is.null(datos)) {
+      message("searching the data cache")
+      return(datos)
+    }
+    intermedio <- x$get()
+    datos <- solve(intermedio, ...)
+    x$setsolve(datos)
+    datos
 }
